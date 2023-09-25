5bcef488131a0cb42dc24c6975cffba32987498f
Ali Bahari <alibahari007@gmail.com>
2023-09-25 20:50:52 +0330

    Add new unannotated images to the dataset

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from time import time, sleep
 from datetime import datetime
@@ -16,8 +18,10 @@ def getNewImageName():
 def capture(chrome: webdriver):
     with open(f"./Unannotated-Dataset/{getNewImageName()}.png", mode = "wb") as img:
         img.write(
-            chrome.find_element(
-                By.ID, os.environ.get("IMG_ID")
+            WebDriverWait(chrome, 30).until(
+                EC.visibility_of_element_located((
+                    By.ID, os.environ.get("IMG_ID")         
+                ))
             ).screenshot_as_png
         )
     print(f"New image added at {datetime.now()}!")
@@ -33,6 +37,7 @@ wait = 10
 options = Options()
 options.add_argument("--headless=new")
 chrome = webdriver.Chrome("./chromedriver.exe", options = options)
+chrome.maximize_window()
 chrome.implicitly_wait(10)
 chrome.get(os.environ.get("URL"))
 
