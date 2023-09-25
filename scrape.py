7f559858d26a11ff6658b72eb25c4f1af456f4a6
Ali Bahari <alibahari007@gmail.com>
2023-09-25 13:40:15 +0330

    Add unannotated image capturing code

--- scrape.py
+++ scrape.py
@@ -1,8 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+from time import time, sleep
+from datetime import datetime
 import os
-import shutil
+from dotenv import load_dotenv
 
-i = 1
+load_dotenv()
 
-for name in os.listdir("./Annotated-Dataset"):
-    shutil.copy(f"./Annotated-Dataset/{name}", f"./Unannotated-Dataset/{i}.png")
-    i += 1
+
+def getNewImageName():
+    return len(os.listdir("./Unannotated-Dataset")) + 1    
+
+def capture(chrome: webdriver):
+    with open(f"./Unannotated-Dataset/{getNewImageName()}.png", mode = "wb") as img:
+        img.write(
+            chrome.find_element(
+                By.ID, os.environ.get("IMG_ID")
+            ).screenshot_as_png
+        )
+    print(f"New image added at {datetime.now()}!")
+
+def renewImage(chrome: webdriver):
+    chrome.find_element(
+        By.ID, os.environ.get("RELOAD_ID")
+    ).click()
+
+
+start = time()
+wait = 10
+options = Options()
+options.add_argument("--headless=new")
+chrome = webdriver.Chrome("./chromedriver.exe", options = options)
+chrome.implicitly_wait(10)
+chrome.get(os.environ.get("URL"))
+
+while(True):
+    capture(chrome)
+    renewImage(chrome)
+    print(f"{len(os.listdir('./Unannotated-Dataset'))} Images in Unannotated Dataset!")
+    sleep( wait - ((time() - start) % wait) )
